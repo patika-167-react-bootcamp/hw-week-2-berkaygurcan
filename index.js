39bc63923458e2970d242954e80452ba1703070f
Berkay Gürcan <46679861+berkaygurcan@users.noreply.github.com>
2022-02-13 18:50:28 +0300

    Gönderi seçildiğined alıcı listesinde değişme özelliği eklendi.

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
 //ilk olarak input değerlerini alalım
 
-const customerList = []
+const customerList = [ {
+    //test datas
+        name: "Berkay",
+        balance: 12000,
+        id: 1
+    },
+    {
+        name: "Fuat",
+        balance: 400,
+        id: 2
+    },
+    {
+        name: "Utku",
+        balance: 9000,
+        id: 3
+    }
+]
+
+//init
+const selectSender = document.getElementById("sender")
+const recipientSender = document.getElementById("recipient")
+
+renderCustomerList()
+renderSenderOption()
+renderRecipientOption()
+
+
 function addCustomer() {
     const customerName = document.getElementById("newCustomerName").value
     const customerBalance = document.getElementById("newCustomerBalance").value
@@ -12,31 +38,46 @@ function addCustomer() {
    
      console.log(customerList)
      renderCustomerList() //render edecek func
-     renderOptions() // optionslarımızı render edecek func
+     renderSenderOption() // optionslarımızı render edecek func
 }
 
-function renderOptions () {
-    const selectSender = document.getElementById("sender")
+function renderSenderOption () {
     selectSender.innerHTML = ""; //içini boşalttık
-
-    const recipientSender = document.getElementById("recipient")
-    recipientSender.innerHTML = ""; //içini boşalttık
+  
     //listemizdeki kullanıcıları option olarak eklememiz gerekli
 
     customerList.forEach(function(customer) { //döngü içerimizde li elementlerimizi oluşturalım
         const newOptionSender = document.createElement("option")
         newOptionSender.innerText = `${customer.name}`
         selectSender.appendChild(newOptionSender);  //tek bir elementi 2elemente atayamıyor muyuz
-        
-        const newOptionRecipient = document.createElement("option")
-        newOptionRecipient.innerText = `${customer.name}`
-        recipientSender.appendChild(newOptionRecipient); 
     })
-
-   
-
-
 }
+
+function renderRecipientOption (exceptCustomerName) {
+    recipientSender.innerHTML = ""; //içini boşalttık
+  
+    //listemizdeki kullanıcıları option olarak eklememiz gerekli
+
+    customerList.forEach(function(customer) { //döngü içerimizde li elementlerimizi oluşturalım
+        if(customer.name !== exceptCustomerName) { //diğer listeden seçili değerimiz bu listede gözükmesin
+            const newOptionRecipient = document.createElement("option")
+            newOptionRecipient.innerText = `${customer.name}`
+            recipientSender.appendChild(newOptionRecipient);  //tek bir elementi 2elemente atayamıyor muyuz
+        }
+        
+    })
+}
+
+//select işleminde değişiklik olunca çalışır.
+selectSender.addEventListener('change', (event) => {
+    console.log(event.target.value);
+    renderRecipientOption(event.target.value)
+})
+
+recipientSender.addEventListener('change', (event) => {
+    console.log(event.target.value);
+})
+
 
 function renderCustomerList() {
     //ul listemizi alalım
