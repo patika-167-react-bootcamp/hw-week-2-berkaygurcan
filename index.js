f18ccda9b18b519d99f2716c2fccbd6e21643819
Berkay Gürcan <46679861+berkaygurcan@users.noreply.github.com>
2022-02-18 18:45:55 +0300

    Revizeler halledildi. Para transferinde gönderici selectinde sorun yaşanıyor.

--- index.js
+++ index.js
@@ -2,16 +2,19 @@
 
 const customerList = []
 let moneyTransferHistory= []
-let copiedmoneyTransferHistory = []
-//initalize application
+const productsList = []
+const basketList = []
+let filter = null
 const selectSender = document.getElementById("sender")
 const selectRecipient = document.getElementById("recipient")
 const selectFilter = document.getElementById("filter")
+const selectBasket = document.getElementById("basket")
 const customerNameInput = document.getElementById("newCustomerName") 
 const customerBalance = document.getElementById("newCustomerBalance") 
 const amountInput = document.getElementById("amount")
 const filterTextInput = document.getElementById("filterText")
 
+let totalPrice = 0 // satış işlemi için
 
 function addCustomer() { 
     //inputlardaki değerleri alalım
@@ -43,7 +46,7 @@ function addCustomer() {
         text: `${customerName} müşterisi sistemde oluşturuldu.`
     })
      
-     console.log("kullanıcı başarılı şekilde eklendi")
+    
      renderHistoryList()
      //inputların içlerini boşaltalım
      customerNameInput.value = ""
@@ -52,11 +55,14 @@ function addCustomer() {
 }
 
 function renderSenderOption () {
+    console.log(" render sender option çalıştım")
     selectSender.innerHTML = ""; //içini boşalttık
+    
   
     //listemizdeki kullanıcıları option olarak eklememiz gerekli
 
     customerList.forEach(function(customer) { //döngü içerisinde li elementlerimizi oluşturalım
+        console.log(customer)
         const newOptionSender = document.createElement("option")
         newOptionSender.innerText = `${customer.name}`
         newOptionSender.setAttribute('data-id',customer.id)
@@ -65,6 +71,7 @@ function renderSenderOption () {
 }
 
 function renderRecipientOption (exceptCustomerName) {
+    
     selectRecipient.innerHTML = ""; //içini boşalttık
   
     //listemizdeki kullanıcıları option olarak eklememiz gerekli
@@ -129,12 +136,12 @@ function customerDelete(e) {
     const index = customerList.findIndex(customer => customer.id === customerId);
     
     const deletedCustomer = customerList.splice(index,1)
-    
+    console.log(deletedCustomer[0].name)
     //history listemizde yazmak için durumu ekleyelim
     moneyTransferHistory.push({
-        customerName: deletedCustomer.name,
+        customerName: deletedCustomer[0].name,
         type: "delete",
-        text: `${deletedCustomer.name} müşterisi sistemden silindi.`
+        text: `${deletedCustomer[0].name} müşterisi sistemden silindi.`
     })
    
     //silme işleminden sonra müşteri listemizi tekrar render edelim(ui için)
@@ -146,12 +153,30 @@ function customerDelete(e) {
     
 }
 
-function renderHistoryList() { 
+function renderHistoryList(targetName = "") { 
     //tamamlanan işlemler bu fonksiyon yardımı ile render edilir.
     const mainLu = document.getElementById("historyList")
     mainLu.innerHTML = ""
+   
 
-    moneyTransferHistory.forEach(function(history) {
+    let resultmoneyTransferHistory; //Filremize göre şekillenicek
+
+        if(filter === "Gönderen") {
+            //text üzerinden gelen değere göre Customer Listemiz üzerinden filtreleme işlemi yapacağız
+            resultmoneyTransferHistory = moneyTransferHistory.filter( history => history.senderCustomer?.name === targetName) 
+            //üst satırdaki senderCustomer? aslında bir kaçış yapmamızı sağlıyor.Eğer sender customer yoksa hata vermek yerine görmezden gelip döngüye devam etmemizi sağlar
+            //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Optional_chaining
+
+        } else if (filter === "Alıcı") {
+            resultmoneyTransferHistory = moneyTransferHistory.filter( history => history.recipientCustomer?.name === targetName) 
+        } else if (filter === "Gönderen & Alıcı"){ //her ikiside olabilir
+            resultmoneyTransferHistory = moneyTransferHistory.filter( history => (history.senderCustomer?.name === targetName) ||  (history.recipientCustomer?.name === targetName)) 
+        } else if (filter === null) { //filtreleme yoksa
+            resultmoneyTransferHistory = moneyTransferHistory //orijinal history datasını aktarıyoruz
+        }
+
+
+    resultmoneyTransferHistory.forEach(function(history) {
         const {type} = history
         //li elementlerinin oluşturulması
         
@@ -203,6 +228,7 @@ function transferHistoryDelete(e) {
         let deletedHistory =moneyTransferHistory.splice(index,1)
         //silme işleminden sonra history listemizi render etmeliyiz
         moneyTransferHistory
+        filter = null //filtremiz varsa kapatıyoruzki listemiz tamamen görünsün
         renderHistoryList()
         revokeTransaction(deletedHistory) //silinen öğemizi gönderdik
     }
@@ -271,45 +297,200 @@ function sendMoney() {
         
     }
     //obje içerisine html yazabilirsin! li leri öyle cagırabilirsin
-    //@todo - olumsuz durumları yap @todo - li etiketini objede yazma durumlarını dene
-
+    //@todo - li etiketini objede yazma durumlarını dene
 }
 
 function filterByCustomer() {
     
     const targetName = filterTextInput.value
-    const filterType = selectFilter.value
+     filter = selectFilter.value //state ekliyoruz aslında
 
     if(!targetName) {
         alert("lütfen filtre değerini giriniz")
         return false
     }
-
-    let result;
-    if(filterType === "Gönderen") {
-        //text üzerinden gelen değere göre Customer Listemiz üzerinden filtreleme işlemi yapacağız
-         result = moneyTransferHistory.filter( history => history.senderCustomer?.name === targetName) 
-        //üst satırdaki senderCustomer? aslında bir kaçış yapmamızı sağlıyor.Eğer sender customer yoksa hata vermek yerine görmezden gelip döngüye devam etmemizi sağlar
-        //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Optional_chaining
-
-    } else if (filterType === "Alıcı") {
-         result = moneyTransferHistory.filter( history => history.recipientCustomer?.name === targetName) 
-    } else { //her ikiside olabilir
-         result = moneyTransferHistory.filter( history => (history.senderCustomer?.name === targetName) ||  (history.recipientCustomer?.name === targetName)) 
-    }
     
-    //ilk olarak history dizimizi korumak için orjinal halini kopyalayım
-    copiedmoneyTransferHistory = moneyTransferHistory.slice() // dizimizi kopyaladık
-    moneyTransferHistory = result.slice() 
-    result.splice(0) //sonrasında result dizisini siliyoruz
-    
-    renderHistoryList() //tekrar render ediyoruz history listemizi
+    renderHistoryList(targetName)
+   
     
 }
 
 function resetFilter() {
-    //orjinal history listemizi tekrar yükleyelim
-    moneyTransferHistory = copiedmoneyTransferHistory.slice() // dizimizi kopyaladık
+    //filtresiz olarak çalıştığı zaten kendi listemiz yüklenecek
+    filter = null
     renderHistoryList() //tekrar render ediyoruz history listemizi
+    
+}
+
+
+// Card ve Sell işlemleri 
+
+function addProduct() {
+
+    //input üzerinden değerlerin alınması
+    const productName = document.getElementById("productName").value
+    const productQuantity = parseInt(document.getElementById("productQuantity").value)
+    const productPrice = parseInt(document.getElementById("productPrice").value)
+
+    //alan kontrolü
+    if(!productName || !productQuantity || !productPrice) {
+        alert("ilgili alanları doldurunuz")
+        return false
+    }
+
+    //listemize ürünümüzü ekleyelim
+
+    productsList.push( { 
+        id: Math.floor(Math.random() * 100), //id oluşturduk,
+        productName,
+        productQuantity,
+        productPrice
+    })
+
+    renderProductsList()
+    
+}
+
+function renderProductsList() { //product listesinin render edilmesinden sorumlu fonksiyon
+
+    //ul listemizi alalım
+    const mainLu = document.getElementById("productsList")
+    mainLu.innerHTML = "" // içini boşaltıyoruz listemizi güncellemeden
+
+    productsList.forEach(function(product) { //döngü içerisinde li elementlerimizi oluşturalım
+
+        //listemize ekleme yaparken div ve span olarak datayı ayırıp daha güzel şekilde gösterelim
+        const newLi = document.createElement("li"); 
+        newLi.className = "list-group-item d-flex justify-content-around align-items-center"; //hizalama işlemleri için
+        newLi.setAttribute('data-id',product.id) // müşteri datasını li etiketine gömdük
+        const productNameDiv = document.createElement("div");
+        productNameDiv.innerText = product.productName;
+        const productQuantitySpan = document.createElement("span");
+        productQuantitySpan.innerText = product.productQuantity + " adet"
+        const productPriceSpan = document.createElement("span");
+        productPriceSpan.innerText = product.productPrice + " tl"
+
+        //ekleme işlemi  için ekle butonu oluşturmamız ve  eklememiz gerekli
+        const newButton = document.createElement("button"); 
+        newButton.innerText = "Ekle"
+        newButton.className = "btn btn-primary btn-sm mb-1" // boostrap üzerinden class verdik
+        newButton.addEventListener('click', addTobasket)
+        
+        //ilgili elementlerin yerleştirilmesi
+        newLi.appendChild(productNameDiv);          
+        newLi.appendChild(productQuantitySpan);
+        newLi.appendChild(productPriceSpan);
+        newLi.appendChild(newButton)
+        mainLu.appendChild(newLi)
+        
+    })
+}
+
+
+function addTobasket(e) {
+    //Ürün yanındaki ekle butonuna tıklanınca burası çalışacak ve sepet listesine ürün eklenecek.
+    targetLi = e.path[1] //path yardımıyla butonu seçtiğimiz yerdeki li elementini aldık
+    
+    const productId = parseInt(targetLi.getAttribute("data-id"))
+    
+    //şimdi productsList içerisinden ilgili id değeri ile eşleşen kaydı bulmamız gerekli
+    const productObj = productsList.find(product => product.id === productId);
+
+    //bu kaydı sepet listesine eklememiz gerekli
+    
+    const basketObj = basketList.find(product => product.productId === productId);
+    
+    //önceden sepette bu ürün varsa 
+    if(basketObj) {
+        basketObj.basketQuantity += 1 //adeti bir adet attırıcaz
+        basketObj.total = basketObj.basketQuantity * basketObj.unitPrice //yeni total fiyatı adet * birim fiyatından buluruz
+    }
+    //önceden sepette bu ürün mevcut değilse
+    else {
+        basketList.push({
+            id: Math.floor(Math.random() * 100), //id oluşturduk,
+            productId: productObj.id,
+            productName : productObj.productName,
+            basketQuantity: 1,
+            unitPrice: productObj.productPrice,
+            total: productObj.productPrice
+        })
+    }
+
+    renderBasketList()
+
+}
+
+function renderBasketList() {
+
+     //ul listemizi alalım
+     const mainLu = document.getElementById("basketList")
+     mainLu.innerHTML = "" // içini boşaltıyoruz listemizi güncellemeden
+ 
+     basketList.forEach(function(product) { //döngü içerisinde li elementlerimizi oluşturalım
+ 
+         //listemize ekleme yaparken div ve span olarak datayı ayırıp daha güzel şekilde gösterelim
+         const newLi = document.createElement("li")
+         newLi.className = "list-group-item d-flex justify-content-center align-items-center"; //hizalama işlemleri için
+         newLi.setAttribute('data-id',product.id) // basket datasını li etiketine gömdük
+         const productBasketDiv = document.createElement("div");
+         productBasketDiv.innerText = `${product.productName} adet: ${product.basketQuantity} birim fiyatı: ${product.unitPrice} toplam: ${product.total}`
+       
+         //ilgili elementlerin yerleştirilmesi
+         newLi.appendChild(productBasketDiv);          
+         mainLu.appendChild(newLi)
+         
+     })
+
+     //toplam ödenecek tutarımızı hesaplayalım
+      totalPrice = basketList.reduce(function (prevValue, currentValue) {
+         return prevValue + currentValue.total
+     }, 0)
+
+     //sayfanın en alt kısmında bütün ürünlerin fiyatlarını göstermemiz gerekli
+     const totalPriceLi = document.createElement("div")
+     totalPriceLi.style = "text-align: center;" // @todo - stillendirmesine bak
+     totalPriceLi.innerText = `Toplam Ödenecek Tutar : ${totalPrice}`
+     mainLu.appendChild(totalPriceLi)
+
+}
+
+function resetBasket() {
+    //basket listemizi sıfırlayalım
+    basketList.splice(0,basketList.length) //listemizi tamamen boşaltırız
+    renderBasketList()
+}
+
+function sell() { //satış yapma işlemi
+
+    //ilgili müşterinin bakiyesinden satış işlemini düşmeliyiz
+    //bu yüzden ilk olarak müşterimize ulaşalım
+    const id = parseInt(selectBasket.selectedOptions[0].getAttribute("data-id"))
+    const customer = customerList.find((customer) => customer.id === id)
+
+    if(!customer) {
+        alert("Müşteri sistemde mevcut değil satış işlemi yapılamıyor.")
+        return false
+    }
+
+    customer.balance -= totalPrice
+   
+    renderCustomerList()
+    resetBasket()
+}
+
+
+function renderSenderOption () {
+    selectBasket.innerHTML = ""; //içini boşalttık
+  
+    //listemizdeki kullanıcıları option olarak eklememiz gerekli
+
+    customerList.forEach(function(customer) { //döngü içerisinde li elementlerimizi oluşturalım
+        const newOptionBasket = document.createElement("option")
+        newOptionBasket.innerText = `${customer.name}`
+        newOptionBasket.setAttribute('data-id',customer.id)
+        selectBasket.appendChild(newOptionBasket);  //tek bir elementi 2elemente atayamıyor muyuz
+    })
+
 
 }
